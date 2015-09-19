3560b96f052d189a1b27503f67fbbc934af90481
Alexander Mantzoukas <alexander.mantzoukas@gmail.com>
2015-09-19 23:28:44 +0200

    Fix the system clock to 8oMHz

--- configuration_bits.c
+++ configuration_bits.c
@@ -36,15 +36,16 @@
 
 // DEVCFG2
 #pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
-#pragma config FPLLMUL = MUL_20         // PLL Multiplier (24x Multiplier)
+#pragma config FPLLMUL = MUL_20         // PLL Multiplier (20x Multiplier)
 #pragma config UPLLIDIV = DIV_12        // USB PLL Input Divider (12x Divider)
 #pragma config UPLLEN = OFF             // USB PLL Enable (Disabled and Bypassed)
-#pragma config FPLLODIV = DIV_4         // System PLL Output Clock Divider (PLL Divide by 1)
+#pragma config FPLLODIV = DIV_1         // System PLL Output Clock Divider (PLL Divide by 1)
+
 
 // DEVCFG1
-#pragma config FNOSC = FRC              // Oscillator Selection Bits (Fast RC Osc without PLL)
-#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Enabled)
-#pragma config IESO = OFF               // Internal/External Switch Over (Enabled)
+#pragma config FNOSC = FRCPLL           // Oscillator Selection Bits (Fast RC Osc with PLL)
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
 #pragma config POSCMOD = OFF            // Primary Oscillator Configuration (Primary osc disabled)
 #pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
 #pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk)
@@ -53,7 +54,7 @@
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Enabled)
 
 // DEVCFG0
-#pragma config DEBUG = ON              // Background Debugger Enable (Debugger is disabled)
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
 #pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
 #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
